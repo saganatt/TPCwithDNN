0e3d3eb8ba14f676c3799c775beb9785821eed47
Maja Kabus <mkabus@cern.ch>
2020-12-18 17:34:04 +0100

    Added weights comparison

--- tpcwithdnn/plot_weights.py
+++ tpcwithdnn/plot_weights.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import sys
 import h5py
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -69,12 +70,41 @@ def print_structure(weight_file_path):
                 print("    Dataset: param.keys():")
                 for k_name in param.keys():
                     weights = param.get(k_name)[:]
-                    print("      {}/{}: {}".format(p_name, k_name, weights.shape)) #, weights))
-                    if (weights.ndim > 1 and 'conv3d_' in p_name and
-                            weights.shape[-1] < 20 and weights.shape[-2] < 20
-                            and weights.shape[-3] < 20):
-                        plot_conv_weights(weights, layer, p_name, k_name)
+                    print("      {}/{}: {}".format(p_name, k_name, weights.shape))
+                    print(weights)
+                    #if (weights.ndim > 1 and 'conv3d_' in p_name and
+                    #        weights.shape[-1] < 20 and weights.shape[-2] < 20
+                    #        and weights.shape[-3] < 20):
+                    #    plot_conv_weights(weights, layer, p_name, k_name)
     finally:
         f.close()
 
-print_structure(sys.argv[1])
+def compare_weights(file1, file2):
+    f1 = h5py.File(file1, 'r')
+    f2 = h5py.File(file2, 'r')
+    try:
+        for (layer1, g1), (layer2, g2) in zip(f1.items(), f2.items()):
+            print("first: {} second: {}".format(layer1, layer2))
+            for p_name1, p_name2 in zip(g1.keys(), g2.keys()):
+                param1 = g1[p_name1]
+                param2 = g2[p_name2]
+                for k_name1, k_name2 in zip(param1.keys(), param2.keys()):
+                    weights1 = param1.get(k_name1)[:]
+                    weights2 = param2.get(k_name2)[:]
+                    print("First: {}/{}: {}".format(p_name1, k_name1, weights1.shape))
+                    print("Second: {}/{}: {}".format(p_name2, k_name2, weights2.shape))
+                    diff_weights = np.abs(weights1 - weights2)
+                    #print("Difference:\n{}".format(diff_weights))
+                    print("Mean difference: {}".format(np.mean(diff_weights)))
+                    #if (weights.ndim > 1 and 'conv3d_' in p_name and
+                    #        weights.shape[-1] < 20 and weights.shape[-2] < 20
+                    #        and weights.shape[-3] < 20):
+                    #    plot_conv_weights(weights, layer, p_name, k_name)
+    finally:
+        f1.close()
+        f2.close()
+
+if len(sys.argv) > 2:
+    compare_weights(sys.argv[1], sys.argv[2])
+else:
+    print_structure(sys.argv[1])
