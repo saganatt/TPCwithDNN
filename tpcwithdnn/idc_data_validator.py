f3d6d72fba3af97717c4fbdbdc6a5fd517a1f710
Maja Kabus <mkabus@cern.ch>
2021-05-06 21:33:19 +0000

    Added ND validation pipeline for IDC

--- tpcwithdnn/idc_data_validator.py
+++ tpcwithdnn/idc_data_validator.py
@@ -1,18 +1,24 @@
 # pylint: disable=missing-module-docstring, missing-function-docstring, missing-class-docstring
 # pylint: disable=too-many-statements, fixme
 import os
+import gzip
+import pickle
+import math
 import numpy as np
 import pandas as pd
 from root_pandas import to_root, read_root  # pylint: disable=import-error, unused-import
+from RootInteractive.Tools.histoNDTools import makeHistogram  # pylint: disable=import-error, unused-import
+from RootInteractive.Tools.makePDFMaps import makePdfMaps  # pylint: disable=import-error, unused-import
 
 from tpcwithdnn.logger import get_logger
 from tpcwithdnn.data_validator import DataValidator
 from tpcwithdnn.data_loader import load_data_original_idc, filter_idc_data
-from tpcwithdnn.data_loader import load_data_derivatives_ref_mean_idc
-from tpcwithdnn.symmetry_padding_3d import SymmetryPadding3d
+from tpcwithdnn.data_loader import load_data_derivatives_ref_mean_idc, load_event_idc
 
 class IDCDataValidator(DataValidator):
     name = "IDC data validator"
+    mean_ids = (0, 27, 36)
+    mean_factors = (0, 1.06, 0.94)
 
     def __init__(self):
         super().__init__()
@@ -130,28 +136,13 @@ class IDCDataValidator(DataValidator):
             df_single_map[column_names[18 + ind_dist * 5]] = mat_fluc_corr[ind_dist, :]
             df_single_map[column_names[19 + ind_dist * 5]] = mat_mean_corr[ind_dist, :]
 
-        # FIXME: Copied from old data validator, to be updated
         if self.config.validate_model:
-            input_single = np.empty((1, self.config.grid_phi, self.config.grid_r,
-                                     self.config.grid_z, self.config.dim_input))
-            index_fill_input = 0
-            if self.config.opt_train[0] == 1:
-                input_single[0, :, :, :, index_fill_input] = \
-                    vec_mean_sc.reshape(self.config.grid_phi, self.config.grid_r,
-                                        self.config.grid_z)
-                index_fill_input = index_fill_input + 1
-            if self.config.opt_train[1] == 1:
-                input_single[0, :, :, :, index_fill_input] = \
-                    vec_fluc_sc.reshape(self.config.grid_phi, self.config.grid_r,
-                                        self.config.grid_z)
-
-            mat_fluc_dist_predict_group = loaded_model.predict(input_single)
-            mat_fluc_dist_predict = np.empty((self.config.dim_output, vec_fluc_sc.size))
-            for ind_dist in range(self.config.dim_output):
-                mat_fluc_dist_predict[ind_dist, :] = \
-                    mat_fluc_dist_predict_group[0, :, :, :, ind_dist].flatten()
-                df_single_map[column_names[19 + ind_dist]] = \
-                    mat_fluc_dist_predict[ind_dist, :]
+            inputs_single, _ = load_event_idc(self.config.dirinput_train,
+                                              [irnd, imean], self.config.input_z_range,
+                                              self.config.output_z_range,
+                                              self.config.opt_predout,
+                                              False, self.config.downsample_frac)
+            df_single_map[column_names[30]] = loaded_model.predict(inputs_single)
 
         df_single_map.to_root(tree_filename, key="validation", mode="a", store_index=False)
 
@@ -177,24 +168,19 @@ class IDCDataValidator(DataValidator):
                                                     "flucCorr" + dist_name,
                                                     "meanCorr" + dist_name])
         if self.config.validate_model:
-            from tensorflow.keras.models import model_from_json # pylint: disable=import-outside-toplevel
-            with open("%s/model_%s_nEv%d.json" % (self.config.dirmodel, self.config.suffix,
-                      self.config.train_events), "r") as json_file:
-                loaded_model_json = json_file.read()
-            loaded_model = \
-                model_from_json(loaded_model_json, {'SymmetryPadding3d' : SymmetryPadding3d})
-            loaded_model.load_weights("%s/model_%s_nEv%d.h5" % \
-                                      (self.config.dirmodel, self.config.suffix,
-                                       self.config.train_events))
-
+            loaded_model = self.model.load_model()
             for dist_name in dist_names:
                 column_names = np.append(column_names, ["flucDist" + dist_name + "Pred"])
         else:
             loaded_model = None
 
-        for imean, mean_factor in zip([0, 2, 5], [1.0, 0.94, 1.06]):
+        for imean, mean_factor in zip(self.mean_ids, self.mean_factors):
             tree_filename = "%s/treeInput_mean%.2f_%s.root" \
                             % (self.config.diroutflattree, mean_factor, self.config.suffix_ds)
+            if self.config.validate_model:
+                tree_filename = "%s/%s/treeValidation_mean%.1f_nEv%d.root" \
+                                % (self.config.diroutflattree, self.config.suffix, mean_factor,
+                                   self.config.train_events)
 
             if os.path.isfile(tree_filename):
                 os.remove(tree_filename)
@@ -221,3 +207,182 @@ class IDCDataValidator(DataValidator):
                         break
 
             self.config.logger.info("Tree written in %s", tree_filename)
+
+    def get_pdf_map_variables_list(self):
+        dist_names_list = np.array(self.config.nameopt_predout) \
+            [np.array([self.config.opt_predout[0], self.config.opt_predout[1],
+                       self.config.opt_predout[2]]) > 0]
+
+        var_list = ['flucSC', 'meanSC', 'derRefMeanSC']
+        for dist_name in dist_names_list:
+            var_list.append('flucDist' + dist_name + 'Pred')
+            var_list.append('flucDist' + dist_name)
+            var_list.append('meanDist' + dist_name)
+            var_list.append('derRefMeanDist' + dist_name)
+            var_list.append('flucDist' + dist_name + 'Diff')
+
+        return var_list
+
+    def create_nd_histogram(self, var, mean_id):
+        """
+        Create nd histograms for given variable and mean id
+        var: string of the variable name
+        mean_id: index of mean map.
+        Only 0 (factor=1.00), 27 (factor=1.06) and 36 (factor=0.94) working.
+        """
+        self.config.logger.info("DataValidator::create_nd_histogram, var = %s, mean_id = %d",
+                                var, mean_id)
+        self.check_mean_id_(mean_id)
+        mean_factor = self.get_mean_factor_(mean_id)
+
+        column_names = ['phi', 'r', 'z', 'deltaSC']
+        diff_index = var.find("Diff")
+        if diff_index == -1:
+            column_names.append(var)
+        else:
+            column_names = column_names + [var[:diff_index], var[:diff_index] + "Pred"]
+
+        df_val = read_root("%s/%s/treeValidation_mean%.1f_nEv%d.root"
+                           % (self.config.diroutflattree, self.config.suffix, mean_factor,
+                              self.config.train_events),
+                           key='validation', columns=column_names)
+        if diff_index != -1:
+            df_val[var] = \
+                df_val[var[:diff_index] + "Pred"] - df_val[var[:diff_index]]
+
+        # Definition string for nd histogram required by makeHistogram function in RootInteractive
+        # 1) variables from data frame
+        # 2) cut selection
+        # 3) histogram name and binning in each dimension
+        # E.g. "var1:var2:var3:#cut_selection>>histo_name(n1,min1,max1,n2,min2,max2,n3,min3,max3)"
+        histo_string = "%s:phi:r:z:deltaSC" % (var) + \
+                       ":#r>0" + \
+                       ">>%s" % (var) + \
+                       "(%d,%.4f,%.4f," % (200, df_val[var].min(), df_val[var].max()) + \
+                       "180,0.0,6.283," + \
+                       "33,83.5,254.5," + \
+                       "40,0,250," + \
+                       "%d,%.4f,%.4f)" % (10, df_val['deltaSC'].min(), df_val['deltaSC'].max())
+        output_file_name = "%s/%s/ndHistogram_%s_mean%.1f_nEv%d.gzip" \
+            % (self.config.dirouthistograms, self.config.suffix, var, mean_factor,
+               self.config.train_events)
+        with gzip.open(output_file_name, 'wb') as output_file:
+            pickle.dump(makeHistogram(df_val, histo_string), output_file)
+        output_file.close()
+        self.config.logger.info("Nd histogram %s written to %s.", histo_string, output_file_name)
+
+    def create_nd_histograms_meanid(self, mean_id):
+        """
+        Create nd histograms for given mean id
+        mean_id: index of mean map.
+        Only 0 (factor=1.00), 27 (factor=1.06) and 36 (factor=0.94) working.
+        """
+        for var in self.config.get_pdf_map_variables_list():
+            self.create_nd_histogram(var, mean_id)
+
+    def create_nd_histograms(self):
+        """
+        Create nd histograms for mean maps with id 0, 27, 36
+        """
+        for mean_id in self.mean_ids:
+            self.create_nd_histograms_meanid(mean_id)
+
+    def create_pdf_map(self, var, mean_id):
+        """
+        Create a pdf map for given variable and mean id
+        var: string of the variable name
+        mean_id: index of mean map.
+        Only 0 (factor=1.00), 27 (factor=1.06) and 36 (factor=0.94) working.
+        """
+        self.config.logger.info("DataValidator::create_pdf_map, var = %s, mean_id = %d",
+                                var, mean_id)
+        self.check_mean_id_(mean_id)
+        mean_factor = self.get_mean_factor_(mean_id)
+
+        input_file_name = "%s/%s/ndHistogram_%s_mean%.1f_nEv%d.gzip" \
+            % (self.config.dirouthistograms, self.config.suffix, var, mean_factor,
+               self.config.train_events)
+        with gzip.open(input_file_name, 'rb') as input_file:
+            histo = pickle.load(input_file)
+
+        output_file_name = "%s/%s/pdfmap_%s_mean%.1f_nEv%d.root" \
+            % (self.config.diroutflattree, self.config.suffix, var, mean_factor,
+               self.config.train_events)
+        dim_var = 0
+        # slices: (start_bin, stop_bin, step, grouping) for each histogram dimension
+        slices = ((0, histo['H'].shape[0], 1, 0),
+                  (0, histo['H'].shape[1], 1, 0),
+                  (0, histo['H'].shape[2], 1, 0),
+                  (0, histo['H'].shape[3], 1, 0),
+                  (0, histo['H'].shape[4], 1, 0))
+        df_pdf_map = makePdfMaps(histo, slices, dim_var)
+        # set the index name to retrieve the name of the variable of interest later
+        df_pdf_map.index.name = histo['name']
+        df_pdf_map.to_root(output_file_name, key=histo['name'], mode='w', store_index=True)
+        self.config.logger.info("Pdf map %s written to %s.", histo['name'], output_file_name)
+
+
+    def create_pdf_maps_meanid(self, mean_id):
+        """
+        Create pdf maps for given mean id
+        mean_id: index of mean map.
+        Only 0 (factor=1.00), 27 (factor=1.06) and 36 (factor=0.94) working.
+        """
+        for var in self.get_pdf_map_variables_list():
+            self.create_pdf_map(var, mean_id)
+
+    def create_pdf_maps(self):
+        """
+        Create pdf maps for mean maps with id 0, 27, 36
+        """
+        for mean_id in self.mean_ids:
+            self.create_pdf_maps_meanid(mean_id)
+
+    def merge_pdf_maps(self, mean_ids=None):
+        """
+        Merge pdf maps for different variables into one file
+        """
+        self.config.logger.info("DataValidator::merge_pdf_maps")
+
+        if mean_ids is None:
+            mean_ids = self.mean_ids
+        mean_factors = [self.mean_factors[mean_ids.index(mean_id)] for mean_id in mean_ids]
+
+        df_merged = pd.DataFrame()
+        for mean_factor in mean_factors:
+            input_file_name_0 = "%s/%s/pdfmap_flucSC_mean%.1f_nEv%d.root" \
+                % (self.config.diroutflattree, self.config.suffix, mean_factor,
+                   self.config.train_events)
+            df = read_root(input_file_name_0, columns="*Bin*")
+            df['fsector'] = df['phiBinCenter'] / math.pi * 9
+            df['meanMap'] = mean_factor
+            for var in self.config.get_pdf_map_variables_list():
+                input_file_name = "%s/%s/pdfmap_%s_mean%.1f_nEv%d.root" \
+                    % (self.config.diroutflattree, self.config.suffix, var, mean_factor,
+                       self.config.train_events)
+                df_temp = read_root(input_file_name, ignore="*Bin*")
+                for col in list(df_temp.keys()):
+                    df[var + '_' + col] = df_temp[col]
+            df_merged = df_merged.append(df, ignore_index=True)
+
+        output_file_name = "%s/%s/pdfmaps_nEv%d.root" \
+            % (self.config.diroutflattree, self.config.suffix, self.config.train_events)
+        df_merged.to_root(output_file_name, key='pdfmaps', mode='w', store_index=False)
+        self.config.logger.info("Pdf maps written to %s.", output_file_name)
+
+    def merge_pdf_maps_meanid(self, mean_id):
+        """
+        Merge pdf maps for given mean id
+        mean_id: index of mean map.
+        Only 0 (factor=1.00), 27 (factor=1.06) and 36 (factor=0.94) working.
+        """
+        self.check_mean_id_(mean_id)
+        self.merge_pdf_maps([mean_id])
+
+    def check_mean_id_(self, mean_id):
+        if mean_id not in self.mean_ids:
+            self.config.logger.error("Code implementation only designed for mean ids 0, 27, 36.")
+            self.config.logger.fatal("Exiting...")
+
+    def get_mean_factor_(self, mean_id):
+        return self.mean_factors[self.mean_ids.index(mean_id)]
